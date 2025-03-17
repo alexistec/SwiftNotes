8e927a740c147ecd77ba68d31582148a31ca974f
aaquinonez <alexisquionez21@gmail.com>
2025-03-17 19:07:28 -0300

    This an example about how create an alert in Swift

---
+++ Notes/Extras/AlertPractices.swift
@@ -0,0 +1,18 @@
+//
+//  AlertPractices.swift
+//  Notes
+//
+//  Created by Alexis Quiñonez on 17/03/2025.
+//
+
+import SwiftUI
+
+struct AlertPractices: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertPractices()
+}
